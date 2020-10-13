d4c460db892edf82b6d0ecf0b1c4819ba153c443
Ankit810 <chorariaankit@gmail.com>
2020-10-13 20:37:47 +0530

    Solved easy

---
+++ Codechef/easy/LifeUniverseAndEverything.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    do    
+    {
+        cin>>n;
+        if(n!=42)
+            cout<<n<<endl;
+        else 
+            return 0;
+    }while(n!=42);
+    return 0;
+}
