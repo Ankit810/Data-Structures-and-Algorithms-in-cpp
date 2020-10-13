47079775d6be989beea3e5ba06e46eda3759736d
Ankit810 <chorariaankit@gmail.com>
2020-10-13 21:50:58 +0530

    Solved in 0.00s

---
+++ Codechef/beginner/AddTwoNumbers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n=0, a, b;
+    cin>>n;
+    while(n>0)
+    {
+        cin>> a>> b;
+        cout<<a+b<<endl;
+        n--;
+    }
+    return 0;
+}
