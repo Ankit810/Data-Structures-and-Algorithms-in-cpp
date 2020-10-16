a8035fc8eb147e78938028ae973b434284855c22
Ankit810 <chorariaankit@gmail.com>
2020-10-16 20:00:09 +0530

    Solved in 0.00s

---
+++ Codechef/beginner/FirstAndLastDigit.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T, sum=0,a,b;
+    string N;
+    cin>>T;
+    while(T--){
+        cin>>N;
+        cout<<(N[0]+N[N.length()-1]-2*'0')<<endl;
+        
+    }
+
+}
