d1686ac8a3944d925715a65a7cc5b6a782f2a2fb
Ankit810 <chorariaankit@gmail.com>
2020-10-19 19:26:08 +0530

    Dolved in 0.00s

---
+++ Codechef/beginner/SecondLargest.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    int T, a[3];
+    cin>>T;
+    while(T--)
+    {
+        for(int i=0; i<3; i++)
+        {
+            cin>>a[i];
+        }
+        sort(a, a+3);
+        cout<<a[1]<<endl;
+    }
+    return 0;
+}
