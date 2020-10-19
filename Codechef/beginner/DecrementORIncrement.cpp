b7531a9668dce494bf3730d09eb10065efb85223
Ankit810 <chorariaankit@gmail.com>
2020-10-19 21:08:27 +0530

    Solved in 0.00s

---
+++ Codechef/beginner/DecrementORIncrement.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    if(N%4==0)
+    cout<<++N<<endl;
+    else
+    cout<<--N<<endl;
+
+    return 0;
+    
+}
